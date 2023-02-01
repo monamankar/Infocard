d7c8bbb58ee4f03fce3c3438175ca419fbae176f
monamankar <monisk2228@gmail.com>
2023-02-01 15:48:53 +0530

    infocard created

---
+++ src/info.js
@@ -0,0 +1,16 @@
+
+function Info(){
+    return(
+
+        <div><p>personal info</p>
+        <img className='round' src="https://randomuser.me/api/portraits/women/79.jpg" alt="user"></img>
+        <h3>Ricky Park</h3>
+        <h6>New York</h6>
+        <p>User Interface Designer and <br/> Web Developer</p>
+        <button>Message</button><button>Follow</button>
+        </div>
+    )
+
+
+}
+export default Info;
---
+++ src/skills.js
@@ -0,0 +1,22 @@
+function Skills(){
+    return(
+        <div><p>skills</p>
+        <div><h6>Skills</h6>
+		<ul>
+			<li>UI / UX</li>
+			<li>Front End Development</li>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>JavaScript</li>
+			<li>React</li>
+			<li>Node</li>
+		</ul>
+	    </div>       
+        </div>
+        
+
+    )
+
+
+}
+export default Skills;
